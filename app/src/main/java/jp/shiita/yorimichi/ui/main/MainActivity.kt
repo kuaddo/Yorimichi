ba567988380b4ff95be05202c11c17fe9aa7759a
shiita0903 <shiita0903@gmail.com>
2018-10-09 18:40:37 +0900

    Setup action bar

--- app/src/main/java/jp/shiita/yorimichi/ui/main/MainActivity.kt
+++ app/src/main/java/jp/shiita/yorimichi/ui/main/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(binding.toolbar)
         binding.setLifecycleOwner(this)
         binding.viewModel = viewModel
 
