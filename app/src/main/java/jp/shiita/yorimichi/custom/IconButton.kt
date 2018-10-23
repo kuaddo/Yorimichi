6558bcdcab2d8789a9aa98220f1f974796e3e7ab
shiita0903 <shiita0903@gmail.com>
2018-10-23 22:39:33 +0900

    Add attrs to IconButton

--- app/src/main/java/jp/shiita/yorimichi/custom/IconButton.kt
+++ app/src/main/java/jp/shiita/yorimichi/custom/IconButton.kt
@@ -1,6 +1,7 @@
 package jp.shiita.yorimichi.custom
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -9,7 +10,8 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import jp.shiita.yorimichi.R
 
-class IconButton(context: Context, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+
+class IconButton(context: Context, attrs: AttributeSet? = null) : LinearLayout(context, attrs) {
     private val imageView: ImageView
     private val textView: TextView
 
@@ -17,6 +19,15 @@ class IconButton(context: Context, attrs: AttributeSet?) : LinearLayout(context,
         LayoutInflater.from(context).inflate(R.layout.view_icon_button, this, true)
         imageView = findViewById(R.id.imageView)
         textView = findViewById(R.id.textView)
+
+        if (attrs != null) {
+            context.theme.obtainStyledAttributes(attrs, R.styleable.IconButton, 0, 0).run {
+                if (hasValue(R.styleable.IconButton_src)) setSrc(getDrawable(R.styleable.IconButton_src))
+                if (hasValue(R.styleable.IconButton_text)) setText(getString(R.styleable.IconButton_text) ?: "")
+                if (hasValue(R.styleable.IconButton_enabled)) isEnabled = getBoolean(R.styleable.IconButton_enabled, true)
+                recycle()
+            }
+        }
     }
 
     override fun setEnabled(enabled: Boolean) {
@@ -24,6 +35,8 @@ class IconButton(context: Context, attrs: AttributeSet?) : LinearLayout(context,
         imageView.isEnabled = enabled
         textView.isEnabled = enabled
     }
+
+    fun setSrc(drawable: Drawable?) = imageView.setImageDrawable(drawable)
 
     fun setSrc(@DrawableRes resId: Int) = imageView.setImageResource(resId)
 
