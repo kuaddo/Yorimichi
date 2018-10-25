4bd629572c33364a7f10883de781a56476985667
shiita0903 <shiita0903@gmail.com>
2018-10-26 00:36:29 +0900

    Show SeekBar value

--- app/src/main/java/jp/shiita/yorimichi/ui/search/SearchViewModel.kt
+++ app/src/main/java/jp/shiita/yorimichi/ui/search/SearchViewModel.kt
@@ -9,21 +9,23 @@ import jp.shiita.yorimichi.util.map
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor() : ViewModel() {
-    val searchEvent: LiveData<Unit>
-        get() = _searchEvent
-    val latLng: LiveData<LatLng>
-        get() = _latLng
-    val selected: LiveData<Boolean>
-        get() = _latLng.map { it != null }
+    val searchEvent       : LiveData<Unit>    get() = _searchEvent
+    val requiredTimeString: LiveData<String>  get() = requiredTimeMinute.map { toTimeString(it) }
+    val latLng            : LiveData<LatLng>  get() = _latLng
+    val selected          : LiveData<Boolean> get() = _latLng.map { it != null }
 
-    private val _searchEvent = SingleUnitLiveEvent()
-    private val _latLng = MutableLiveData<LatLng>()
+    val requiredTimeMinute = MutableLiveData<Int>().apply { value = 80 }
+
+    private val _searchEvent        = SingleUnitLiveEvent()
+    private val _latLng             = MutableLiveData<LatLng>()
 
     fun search() {
         _searchEvent.call()
     }
 
-    fun select(latLng: LatLng) {
-        _latLng.postValue(latLng)
-    }
+    fun select(latLng: LatLng) = _latLng.postValue(latLng)
+
+    private fun toTimeString(timeMinute: Int): String =
+            if (timeMinute < 60) "%02d分".format(timeMinute)
+            else "%d時間%02d分".format(timeMinute / 60, timeMinute % 60)
 }
