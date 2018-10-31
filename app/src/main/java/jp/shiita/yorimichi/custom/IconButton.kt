1c8bd662d7bc7bdbf3cad36cf8d5c67b60fa996d
shiita0903 <shiita0903@gmail.com>
2018-10-31 15:39:51 +0900

    Add small IconButton

--- app/src/main/java/jp/shiita/yorimichi/custom/IconButton.kt
+++ app/src/main/java/jp/shiita/yorimichi/custom/IconButton.kt
@@ -1,14 +1,17 @@
 package jp.shiita.yorimichi.custom
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.support.annotation.DrawableRes
 import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import jp.shiita.yorimichi.R
+import jp.shiita.yorimichi.util.getBitmap
 
 
 class IconButton(context: Context, attrs: AttributeSet? = null) : LinearLayout(context, attrs) {
@@ -24,6 +27,8 @@ class IconButton(context: Context, attrs: AttributeSet? = null) : LinearLayout(c
             context.theme.obtainStyledAttributes(attrs, R.styleable.IconButton, 0, 0).run {
                 if (hasValue(R.styleable.IconButton_src)) setSrc(getDrawable(R.styleable.IconButton_src))
                 if (hasValue(R.styleable.IconButton_text)) setText(getString(R.styleable.IconButton_text) ?: "")
+                if (hasValue(R.styleable.IconButton_iconSize)) setIconSize(getDimensionPixelSize(R.styleable.IconButton_iconSize, 0))
+                if (hasValue(R.styleable.IconButton_textSize)) setTextSize(getDimensionPixelSize(R.styleable.IconButton_textSize, 0))
                 if (hasValue(R.styleable.IconButton_enabled)) isEnabled = getBoolean(R.styleable.IconButton_enabled, true)
                 if (hasValue(R.styleable.IconButton_marginCenter)) textView.layoutParams = textView.layoutParams.also {
                     (it as LayoutParams).marginStart = getLayoutDimension(R.styleable.IconButton_marginCenter, 0)
@@ -46,4 +51,12 @@ class IconButton(context: Context, attrs: AttributeSet? = null) : LinearLayout(c
     fun setText(text: String) {
         textView.text = text
     }
+
+    fun setIconSize(size: Int) {
+        val bitmap = imageView.drawable.getBitmap()
+        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, size, size, false)
+        imageView.setImageBitmap(resizedBitmap)
+    }
+
+    fun setTextSize(size: Int) = textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, size.toFloat())
 }
