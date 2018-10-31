1b0848c2743ddbb0225fd17936105acc1b1b64fe
shiita0903 <shiita0903@gmail.com>
2018-10-31 23:20:10 +0900

    Add large pin to map

--- app/src/main/java/jp/shiita/yorimichi/ui/searchresult/SearchResultFragment.kt
+++ app/src/main/java/jp/shiita/yorimichi/ui/searchresult/SearchResultFragment.kt
@@ -3,8 +3,10 @@ package jp.shiita.yorimichi.ui.searchresult
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -13,15 +15,13 @@ import android.view.ViewGroup
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CircleOptions
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import dagger.android.support.DaggerFragment
 import jp.shiita.yorimichi.R
 import jp.shiita.yorimichi.data.UserInfo
 import jp.shiita.yorimichi.databinding.FragSearchResultBinding
 import jp.shiita.yorimichi.ui.main.MainViewModel
+import jp.shiita.yorimichi.util.getBitmap
 import jp.shiita.yorimichi.util.observe
 import javax.inject.Inject
 
@@ -37,6 +37,8 @@ class SearchResultFragment : DaggerFragment() {
     private val latLng: LatLng? = UserInfo.latLng
     private var map: GoogleMap? = null
     private lateinit var markers: List<Marker?>
+    private lateinit var smallDescriptor: BitmapDescriptor
+    private lateinit var largeDescriptor: BitmapDescriptor
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.frag_search_result, container, false)
@@ -62,9 +64,9 @@ class SearchResultFragment : DaggerFragment() {
                     super.onScrolled(recyclerView, dx, dy)
                     val first = layoutManager.findFirstVisibleItemPosition()
                     val last = layoutManager.findLastVisibleItemPosition()
-                    if (first > oldFirst) (oldFirst until first).forEach { markers[it]?.alpha = 0f }    // invisible
-                    if (last < oldLast) (last + 1..oldLast).forEach      { markers[it]?.alpha = 0f }    // invisible
-                    (first..last).forEach                                { markers[it]?.alpha = 1f }    // visible
+                    if (first > oldFirst) (oldFirst until first).forEach { markers[it]?.setIcon(smallDescriptor) }    // invisible
+                    if (last < oldLast) (last + 1..oldLast).forEach      { markers[it]?.setIcon(smallDescriptor) }    // invisible
+                    (first..last).forEach                                { markers[it]?.setIcon(largeDescriptor) }    // visible
 
                     oldFirst = first
                     oldLast = last
@@ -72,21 +74,17 @@ class SearchResultFragment : DaggerFragment() {
             })
         }
 
+        initDescriptor()
         initMap()
         observe()
     }
 
-    fun observe() {
-        viewModel.places.observe(this) { places ->
-            searchResultAdapter.reset(places)
-            markers = places.map {
-                val marker = MarkerOptions()
-                        .position(it.latLng)
-                        .alpha(0f)
-                map?.addMarker(marker)
-            }
-        }
-        viewModel.zoomBounds.observe(this) { map?.moveCamera(CameraUpdateFactory.newLatLngBounds(it, 0)) }
+    private fun initDescriptor() {
+        val pinDrawable = ResourcesCompat.getDrawable(resources, R.drawable.ic_pin_large, null)!!
+        val largeBitmap = pinDrawable.getBitmap(ResourcesCompat.getColor(resources, R.color.colorPrimary, null))
+        val smallBitmap = Bitmap.createScaledBitmap(largeBitmap, largeBitmap.width / 2, largeBitmap.height / 2, false)
+        smallDescriptor = BitmapDescriptorFactory.fromBitmap(smallBitmap)
+        largeDescriptor = BitmapDescriptorFactory.fromBitmap(largeBitmap)
     }
 
     private fun initMap() {
@@ -102,6 +100,19 @@ class SearchResultFragment : DaggerFragment() {
 
             viewModel.searchPlaces(latLng.latitude, latLng.longitude)
         }
+    }
+
+    private fun observe() {
+        viewModel.places.observe(this) { places ->
+            searchResultAdapter.reset(places)
+            markers = places.map {
+                val marker = MarkerOptions()
+                        .position(it.latLng)
+                        .icon(smallDescriptor)
+                map?.addMarker(marker)
+            }
+        }
+        viewModel.zoomBounds.observe(this) { map?.moveCamera(CameraUpdateFactory.newLatLngBounds(it, 0)) }
     }
 
     companion object {
--- app/src/main/java/jp/shiita/yorimichi/util/Extension.kt
+++ app/src/main/java/jp/shiita/yorimichi/util/Extension.kt
@@ -1,21 +1,15 @@
 package jp.shiita.yorimichi.util
 
-import android.annotation.TargetApi
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.Transformations
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.PorterDuff
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.graphics.drawable.VectorDrawable
 import android.location.Location
-import android.os.Build
 import android.support.annotation.ColorInt
 import android.support.annotation.IdRes
-import android.support.graphics.drawable.VectorDrawableCompat
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.graphics.drawable.DrawableCompat
@@ -27,27 +21,9 @@ import jp.shiita.yorimichi.BuildConfig
 import jp.shiita.yorimichi.data.UserInfo
 import java.io.ByteArrayOutputStream
 
+
 val Location.latLng
     get() = LatLng(latitude, longitude)
-
-val VectorDrawable.bitmap: Bitmap
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    get() {
-        val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        setBounds(0, 0, canvas.width, canvas.height)
-        draw(canvas)
-        return bitmap
-    }
-
-val VectorDrawableCompat.bitmap: Bitmap
-    get() {
-        val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        setBounds(0, 0, canvas.width, canvas.height)
-        draw(canvas)
-        return bitmap
-    }
 
 fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T) -> Unit) =
         observe(owner, Observer<T> { if (it != null) observer(it) })
@@ -85,17 +61,34 @@ fun AdView.loadAd() {
 
 fun ByteArray.toBase64(): String = Base64.encodeToString(this, Base64.NO_WRAP)
 
+/**
+ * [Bitmap]を取得しない場合に利用するTint
+ */
 fun Drawable.setTintCompat(@ColorInt color: Int): Drawable = DrawableCompat.wrap(this).mutate().also {
     DrawableCompat.setTint(it, color)
     DrawableCompat.setTintMode(it, PorterDuff.Mode.SRC_IN)
 }
 
-@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 fun Drawable.getBitmap(): Bitmap = when(this) {
     is BitmapDrawable -> bitmap
-    is VectorDrawableCompat -> bitmap
-    is VectorDrawable -> bitmap
-    else -> error("invalid drawable type")
+    else -> {
+        val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        setBounds(0, 0, canvas.width, canvas.height)
+        draw(canvas)
+        bitmap
+    }
+}
+
+/**
+ * [Bitmap]を取得する場合に利用するTint
+ */
+fun Drawable.getBitmap(@ColorInt color: Int): Bitmap {
+    val bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val paint = Paint().apply { colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_IN) }
+    val canvas = Canvas(bitmap)
+    canvas.drawBitmap(getBitmap(), 0f, 0f, paint)
+    return bitmap
 }
 
 /**
